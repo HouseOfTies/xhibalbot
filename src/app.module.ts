57095182dd7c9c61e53374aaea247c6d47ea58f4
Seventty <zeroseventty@gmail.com>
2025-02-18 12:01:56 -0400

    feat: reorganize module imports in app.module.ts and add missing modules in bot.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,5 @@
-import { UserModule } from './Modules/user/user.module';
 import { I18nService } from './share/services/i18n/i18n.service';
-import { MineModule } from './Modules/Mine/mine.module';
 import { RedisCooldownModule } from './cache/rediscooldown.module';
-import { GreeterModule } from './Modules/greeter/greeter.module';
 import { ShareModule } from './share/share.module';
 import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
@@ -13,9 +10,6 @@ import { BotModule } from './bot/bot.module';
 
 @Module({
   imports: [
-    MineModule,
-    GreeterModule,
-    UserModule,
     ShareModule,
     DatabaseModule,
     ConfigModule.forRoot({
--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -5,9 +5,14 @@ import { UserService } from '../Modules/user/user.service';
 import { sessionMiddleware } from '../middleware/session.middleware';
 import { LoggerMiddleware } from '../middleware/logger.middleware';
 import { RegisterMiddleware } from 'src/middleware/register.middleware';
+import { MineModule } from 'src/Modules/Mine/mine.module';
+import { GreeterModule } from 'src/Modules/greeter/greeter.module';
 
 @Module({
   imports: [
+    UserModule,
+    MineModule,
+    GreeterModule,
     UserModule,
     TelegrafModule.forRootAsync({
       botName: 'XhibalbaBot',
