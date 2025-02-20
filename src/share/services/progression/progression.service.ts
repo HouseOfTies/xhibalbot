56a7f601003a828328cc0b93fea1245fe3e542e9
Seventty <zeroseventty@gmail.com>
2025-02-19 21:16:32 -0400

    feat: add progression messages for rewards and level-ups; implement i18n support for user notifications

--- src/share/services/progression/progression.service.ts
+++ src/share/services/progression/progression.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/Modules/user/user.service';
 import { Context } from 'telegraf';
 import { ExperienceService } from '../experience/experience.service';
+import { I18nService } from 'src/share/services/i18n/i18n.service';
 
 @Injectable()
 export class ProgressionService {
+  /* TODO: Fix values response in i18n, it seems like exp: {12} */
   constructor(
     public readonly userService: UserService,
     private readonly experienceService: ExperienceService,
+    private readonly i18nService: I18nService,
   ) {}
 
   async updateProgress(
@@ -20,6 +23,8 @@ export class ProgressionService {
     const user = await this.userService.getUser(userId);
     if (!user) return;
 
+    const lang = user.language || 'en';
+
     user.exp += exp;
     user.goldCoins += gold;
     user.miningExp += miningExp;
@@ -31,7 +36,13 @@ export class ProgressionService {
     if (user.exp >= requiredExp) {
       user.level += 1;
       user.exp = 0;
-      levelUpMessage = `🎉 ¡Felicidades! Has subido al **Nivel ${user.level}**!`;
+      levelUpMessage = this.i18nService.translate(
+        lang,
+        'commands.progression.levelUp',
+        {
+          level: user.level,
+        },
+      );
     }
 
     const requiredMiningExp = this.experienceService.getRequiredMiningExp(
@@ -40,19 +51,27 @@ export class ProgressionService {
     if (user.miningExp >= requiredMiningExp) {
       user.miningLevel += 1;
       user.miningExp = 0;
-      miningLevelUpMessage = `⛏️ ¡Tu nivel de minería ha subido a **${user.miningLevel}**!`;
+      miningLevelUpMessage = this.i18nService.translate(
+        lang,
+        'commands.progression.miningLevelUp',
+        { miningLevel: user.miningLevel },
+      );
     }
 
     await user.save();
 
-    const rewardMessage = `
-🎖 **Recompensa obtenida:**
-   - ✨ **EXP:** ${exp}
-   - ⛏️ **Mining EXP:** ${miningExp}
-   - 🪙 **Gold Coins:** ${gold}
-    `;
+    const rewardMessage = this.i18nService.translate(
+      lang,
+      'commands.progression.reward',
+      {
+        exp: exp,
+        miningExp: miningExp,
+        gold: gold,
+      },
+    );
 
     await ctx.reply(rewardMessage, { parse_mode: 'Markdown' });
+    console.log(rewardMessage);
     if (levelUpMessage)
       await ctx.reply(levelUpMessage, { parse_mode: 'Markdown' });
     if (miningLevelUpMessage)
